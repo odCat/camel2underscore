34937b7786ac88ff21d35f6f1abcbb23e253180f
Mihai Gătejescu <gus666xe@gmail.com>
2020-05-07 19:34:08 +0300

    Change out to output

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,19 +1,19 @@
 #!python3
 
 input = ["TestInput1", "test2Input", "1test"]
-out = ''
+output = ''
 
 for i in input:
     for j in i:
         if j in ['0','1','2','3','4','5','6','7','8','9']:
-            out = i[:i.find(j)]
-            out += '_' + j + '_'
-            out += i[i.find(j) + 1:]
-    if out[0] == "_":
-        out = out[1:]
-    if out[-1] == "_":
-        out = out[:-1]
-    print(out)
+            output = i[:i.find(j)]
+            output += '_' + j + '_'
+            output += i[i.find(j) + 1:]
+    if output[0] == "_":
+        output = output[1:]
+    if output[-1] == "_":
+        output = output[:-1]
+    print(output)
 
 # TODO
 # Fis the multiple digits number
