f9d40edf590c150bcecd7089b1e6d4b322e015b2
Mihai Gătejescu <gus666xe@gmail.com>
2020-05-07 19:32:05 +0300

    Add underscores around digits

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,3 +1,16 @@
 #!python3
 
-input = ["TestInput1", "test2Input"]
+input = ["TestInput1", "test2Input", "1test"]
+out = ''
+
+for i in input:
+    for j in i:
+        if j in ['0','1','2','3','4','5','6','7','8','9']:
+            out = i[:i.find(j)]
+            out += '_' + j + '_'
+            out += i[i.find(j) + 1:]
+    if out[0] == "_":
+        out = out[1:]
+    if out[-1] == "_":
+        out = out[:-1]
+    print(out)
