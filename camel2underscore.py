559441ca4f5bc507d21a6f6860d7422ceedfa640
Mihai Gătejescu <gus666xe@gmail.com>
2020-05-07 21:45:38 +0300

    Add underline before uppercase

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,14 +1,18 @@
 #!python3
 
-input = ["TestInput1", "test2Input", "1test"]
-output = ''
+input = ["TestInput1", "Test2Input", "1Test", "Test22Test", "TestThisTest"]
 
 for i in input:
+    output = ''
+
     for j in i:
         if j > '0' and j < '9':
-            output = i[:i.find(j)]
-            output += '_' + j + '_'
-            output += i[i.find(j) + 1:]
+            output += '_' + j
+        elif j.isupper():
+            output += '_' + j
+        else:
+            output += j
+
     if output[0] == "_":
         output = output[1:]
     if output[-1] == "_":
