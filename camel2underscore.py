f50c801c6dc5be34a30579cc3f3fa43d6a9bf3b0
Mihai Gătejescu <gus666xe@gmail.com>
2020-05-08 14:22:55 +0300

    Add digit and uppercase flags

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,13 +1,23 @@
 #!python3
 
-input = ["TestInput1", "Test2Input", "1Test", "Test22Test", "TestThisTest"]
+input = ["TestInput1", "Test2Input", "1Test", "Test22Test", "TestThisTest", "Is21NOT"]
+digit_flag = False
+upper_flag = False
 
 for i in input:
     output = ''
 
     for j in i:
-        if j.isdigit() or j.isupper():
-            output += '_'
+        if j.isdigit():
+            if not digit_flag:
+                output += '_'
+            digit_flag = True
+        elif j.isupper():
+            if not upper_flag:
+                output += '_'
+            upper_flag = True
+        else:
+            digit_flag = upper_flag = False
         output += j
 
     if output[0] == "_":
