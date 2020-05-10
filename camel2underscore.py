d86a554e3e605e2d6b31b5e00e911489d55c453e
Mihai Gătejescu <gus666xe@gmail.com>
2020-05-11 01:05:55 +0300

    Add make_printable function

--- camel2underscore.py
+++ camel2underscore.py
@@ -4,7 +4,14 @@ input_values = ["TestInput1", "Test2Input", "1Test", "Test22Test", "TestThisTest
 digit_flag = False
 upper_flag = False
 
-for i in input:
+def make_printable(output):
+    if output[0] == "_":
+        output = output[1:]
+    if output[-1] == "_":
+        output = output[:-1]
+    print(output.lower())
+
+for i in input_values:
     output = ''
 
     for j in i:
@@ -20,11 +27,7 @@ for i in input:
             digit_flag = upper_flag = False
         output += j
 
-    if output[0] == "_":
-        output = output[1:]
-    if output[-1] == "_":
-        output = output[:-1]
-    print(output.lower())
+    make_printable(output)
 
 # TODO
 # refactor
