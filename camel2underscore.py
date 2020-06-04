7bf9483d292f8cfec95ff0c4064d09bdd7ccfa43
Mihai Gătejescu <gus666xe@gmail.com>
2020-06-04 22:40:27 +0300

    Move the conversion into a function

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,8 +1,5 @@
 #!python3
 
-previous_was_digit = False
-previous_was_uppercase = False
-inside_acronym = False
 
 def remove_newline(input):
     output = []
@@ -30,30 +27,39 @@ def make_printable(output):
         output = remove_last_character(output)
     return output.lower()
 
-input_values = remove_newline(read_input_from_file())
-for value in input_values:
+def convert(input_values):
+    previous_was_digit = False
+    previous_was_uppercase = False
+    inside_acronym = False
+
     output = ''
+    for value in input_values:
+        line = ''
 
-    for char in value:
-        if char.isdigit():
-            if not previous_was_digit:
-                output += '_'
-            previous_was_digit = True
-        elif char.isupper():
-            if not previous_was_uppercase:
-                output += '_'
-            else:
-                inside_acronym = True
-            previous_was_uppercase = True
-        elif char.islower():
-            if inside_acronym:
-                output = output[:-1] + '_' + output[-1:]
-            previous_was_uppercase = False
-            previous_was_digit = False
-            inside_acronym = False
-        output += char
+        for char in value:
+            if char.isdigit():
+                if not previous_was_digit:
+                    line += '_'
+                previous_was_digit = True
+            elif char.isupper():
+                if not previous_was_uppercase:
+                    line += '_'
+                else:
+                    inside_acronym = True
+                previous_was_uppercase = True
+            elif char.islower():
+                if inside_acronym:
+                    line = line[:-1] + '_' + line[-1:]
+                previous_was_uppercase = False
+                previous_was_digit = False
+                inside_acronym = False
+            line += char
+            line = make_printable(line)
+        output += line + '\n'
+    return output[:-1]
 
-    print(make_printable(output))
+input_values = remove_newline(read_input_from_file())
+print(convert(input_values))
 
 # TODO
 # refactor
