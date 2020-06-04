fecb945d94ad06f5fecdc917f1705f77203d9040
Mihai Gătejescu <gus666xe@gmail.com>
2020-06-04 21:55:37 +0300

    Move the print out of make_printable

--- camel2underscore.py
+++ camel2underscore.py
@@ -1,9 +1,10 @@
 #!python3
 
 input_values = ["TestInput1", "Test2Input", "Test22Test", "TestThisTest", "1NOTIs21"]
+
 previous_was_digit = False
 previous_was_uppercase = False
-inside_word = False
+inside_acronym = False
 
 def first_char_is_underline(output):
     return output[0] == '_'
@@ -19,7 +20,7 @@ def make_printable(output):
         output = remove_first_character(output)
     if last_char_is_underline(output):
         output = remove_last_character(output)
-    print(output.lower())
+    return output.lower()
 
 for value in input_values:
     output = ''
@@ -33,17 +34,17 @@ for value in input_values:
             if not previous_was_uppercase:
                 output += '_'
             else:
-                inside_word = True
+                inside_acronym = True
             previous_was_uppercase = True
         elif char.islower():
-            if inside_word:
+            if inside_acronym:
                 output = output[:-1] + '_' + output[-1:]
             previous_was_uppercase = False
             previous_was_digit = False
-            inside_word = False
+            inside_acronym = False
         output += char
 
-    make_printable(output)
+    print(make_printable(output))
 
 # TODO
 # refactor
