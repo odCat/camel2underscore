89ed3cce1bb03e8a0ccda4043af7c1c5d4037311
Mihai Gătejescu <gus666xe@gmail.com>
2021-02-19 22:02:08 +0200

    Add copyright and license notices

--- gui.py
+++ gui.py
@@ -1,5 +1,19 @@
 #!python
 
+#   Copyright 2020, 2021 Mihai Gătejescu
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import tkinter as tk
 
 root = tk.Tk()
@@ -15,8 +29,8 @@ frame2 = tk.Frame(root)
 frame2.pack(side = 'left', fill = 'both', expand = True)
 scrollbar = tk.Scrollbar(frame2)
 scrollbar.pack(side = 'right', fill = 'y')
-text = tk.Text(frame2, wrap = 'word', yscrollcommand = scrollbar.set)
+text = tk.Text(frame2, wrap = 'word')#, yscrollcommand = scrollbar.set)
 text.pack(side = 'left', fill = 'both', expand = True)
-scrollbar['command'] = text.yview
+#scrollbar['command'] = text.yview
 
 root.mainloop()
